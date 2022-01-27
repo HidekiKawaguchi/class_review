18cc24a1e79437600ffa5c5726bf588f76eea651
johnpori <h.kawaguchi0610@gmail.com>
2022-01-27 22:55:18 +0900

    1st commit

---
+++ customer.py
@@ -0,0 +1,54 @@
+
+
+"""
+基本課題
+下記のコードが期待通り動作するようなCustomerクラスを実装してください
+
+
+C-1. フルネームを取得できる
+ken = Customer(first_name="Ken", family_name="Tanaka")
+ken.full_name()  # "Ken Tanaka" という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford")
+tom.full_name()  # "Tom Ford" という値を返す
+
+
+
+C-2. 年齢という概念の追加
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.age  # 15 という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+tom.age # 57 という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.age # 73 という値を返す
+
+
+
+C-3. 年齢に応じた適切な入場料(entry_fee)を計算できる
+料金の計算ルール
+こども料金(20歳未満): 1000円
+おとな料金(20歳以上65歳未満): 1500円
+シニア料金(65歳以上): 1200円
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.entry_fee()  # 1000 という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+tom.entry_fee() # 1500 という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.entry_fee() # 1200 という値を返す
+
+
+
+C-4. 単一の顧客情報をCSV形式で取得できる
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+ken.info_csv()  # "Tom Ford,57,1500" という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
+"""
