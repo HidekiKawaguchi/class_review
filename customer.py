fb4fe3252bfb80e2d2ae85aa4af87e29aa1afa35
johnpori <h.kawaguchi0610@gmail.com>
2022-01-28 07:39:40 +0900

    課題C完成

--- customer.py
+++ customer.py
@@ -1,54 +1,52 @@
+# クラス(データ型)の定義
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name    # インスタンス変数
+        self.family_name = family_name  # インスタンス変数
+        self.age = age  # インスタンス変数
+
+    def full_name(self):
+        return f"{self.first_name} {self.family_name}"
+
+    def entry_fee(self):
+        if self.age >= 65:
+            return "1200"
+        elif self.age >= 20:
+            return "1500"
+        else:
+            return "1000"
+
+    def info_csv(self):
+        return f"{self.full_name()},{self.age},{self.entry_fee()}"
 
 
-"""
-基本課題
-下記のコードが期待通り動作するようなCustomerクラスを実装してください
+# 実体化(インスタンス化)
+# C-1. フルネームを取得できる
 
 
-C-1. フルネームを取得できる
-ken = Customer(first_name="Ken", family_name="Tanaka")
-ken.full_name()  # "Ken Tanaka" という値を返す
-
-tom = Customer(first_name="Tom", family_name="Ford")
-tom.full_name()  # "Tom Ford" という値を返す
-
-
-
-C-2. 年齢という概念の追加
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-ken.age  # 15 という値を返す
-
-tom = Customer(first_name="Tom", family_name="Ford", age= 57)
-tom.age # 57 という値を返す
-
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-ieyasu.age # 73 という値を返す
 
 
-
-C-3. 年齢に応じた適切な入場料(entry_fee)を計算できる
-料金の計算ルール
-こども料金(20歳未満): 1000円
-おとな料金(20歳以上65歳未満): 1500円
-シニア料金(65歳以上): 1200円
-ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-ken.entry_fee()  # 1000 という値を返す
-
-tom = Customer(first_name="Tom", family_name="Ford", age= 57)
-tom.entry_fee() # 1500 という値を返す
-
-ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-ieyasu.entry_fee() # 1200 という値を返す
+# C-1出力(フルネームを取得できる)
+print(ken.full_name())  # "Ken Tanaka" という値を返す
+print(tom.full_name())  # "Tom Ford" という値を返す
 
 
+# C-2出力(年齢という概念の追加)
+print(ken.age)  # 15 という値を返す
+print(tom.age)  # 57 という値を返す
+print(ieyasu.age)  # 73 という値を返す
 
-C-4. 単一の顧客情報をCSV形式で取得できる
-ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
 
-tom = Customer(first_name="Tom", family_name="Ford", age= 57)
-ken.info_csv()  # "Tom Ford,57,1500" という値を返す
+# C-3出力 年齢に応じた適切な入場料(entry_fee)を計算できる
+print(ken.entry_fee())  # 1000 という値を返す
+print(tom.entry_fee())  # 1500 という値を返す
+print(ieyasu.entry_fee())  # 1200 という値を返す
 
-ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
-"""
+
+# C-4. 単一の顧客情報をCSV形式で取得できる
+print(ken.info_csv())  # "Ken Tanaka,15,1000" という値を返す
+print(ken.info_csv())  # "Tom Ford,57,1500" という値を返す
+print(ieyasu.info_csv())  # "Ieyasu Tokugawa,73,1200" という値を返す
